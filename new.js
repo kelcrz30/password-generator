01289fc21cd5f8b42cb5ebf077dbb541f03a61b9
Kel <davenykel47@gmail.com>
2025-05-04 15:43:27 +0800

    Initial commit - password generator project

---
+++ new.js
@@ -0,0 +1,39 @@
+function enterPass(){
+    let inputPassword = parseInt(document.getElementById("length").value);
+    let uppercaseBtn = document.getElementById("uppercaseBtn").checked
+    let lowercaseBtn = document.getElementById("lowercaseBtn").checked
+    let numberBtn = document.getElementById("numberBtn").checked
+    let symbolBtn = document.getElementById("symbolBtn").checked
+    let result = document.getElementById("result")
+    let output = "";
+
+
+    if (inputPassword < 1) {
+        result.textContent = "Please enter a number greater than 1.";
+		return;
+    }
+    if (uppercaseBtn) {
+        output += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (lowercaseBtn) {
+        output += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (numberBtn) {
+        output += "0123456789";
+    }
+    if (symbolBtn) {
+        output += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+    }
+    if (output === "") {
+        result.textContent = "Please select at least one character type.";
+        return;
+    }
+
+    let password = "";
+    for (let i = 0; i < inputPassword; i++) {
+    const randomPass = Math.floor(Math.random() * output.length)    
+    password += output[randomPass]
+    }
+    
+    result.textContent = `Your generated password is ${password}`
+}
